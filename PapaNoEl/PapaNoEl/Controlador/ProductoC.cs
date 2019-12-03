d9d1e3876ce7e200137704cee544237fc65bc880
VSJose <joseeph159@gmail.com>
2019-12-03 13:08:35 -0400

    product

---
+++ PapaNoEl/PapaNoEl/Controlador/ProductoC.cs
@@ -0,0 +1,68 @@
+﻿using PapaNoEl.Modelo.Datos;
+using PapaNoEl.Modelo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaNoEl.Controlador
+{
+    class ProductoC
+    {
+        ProductoD _db = new ProductoD();
+        public bool GuardarCambios(Producto nuevoProducto)
+        {
+            try
+            {
+                Producto tblProducto = new Producto();
+
+                tblProducto.descripcion = nuevoProducto.descripcion;
+                tblProducto.tipo = nuevoProducto.tipo;
+                tblProducto.precio = nuevoProducto.precio;
+                tblProducto.stock = nuevoProducto.stock;
+                return _db.Adicionar(tblProducto) > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Modificar(Producto nuevoProducto)
+        {
+            try
+            {
+                Producto tblProducto = new Producto();
+
+                tblProducto.descripcion = nuevoProducto.descripcion;
+                tblProducto.tipo = nuevoProducto.tipo;
+                tblProducto.precio = nuevoProducto.precio;
+                tblProducto.stock = nuevoProducto.stock;
+
+                return _db.Editar(tblProducto) > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(string id)
+        {
+            try
+            {
+                return _db.Eliminar(id) > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Producto> MostrarDatos()
+        {
+            return _db.ObtenerTodo().ToList();
+        }
+    }
+}
---
+++ PapaNoEl/PapaNoEl/Modelo/Datos/ProductoD.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PapaNoEl.Modelo.Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PapaNoEl.Modelo.Datos
+{
+    class ProductoD:Query
+    {
+        private string seleccionarTodo;
+        private string insertar;
+        private string actualizar;
+        private string eliminar;
+
+        public ProductoD()
+        {
+            seleccionarTodo = "select * from Productos";
+            insertar = "insert into Productos values(@descripcion,@tipo,@precio,@stock)";
+            actualizar = "update Productos set Descripcion=@descripcion,Tipo=@tipo,Precio=@precio,Stock=@stock where IdProducto=@idproducto";
+            eliminar = "delete from Productos where IdProducto=@idproducto";
+        }
+
+        public int Adicionar(Producto entidad)
+        {
+            parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@descripcion", entidad.descripcion));
+            parametros.Add(new SqlParameter("@tipo", entidad.tipo));
+            parametros.Add(new SqlParameter("@precio", entidad.precio));
+            parametros.Add(new SqlParameter("@stock", entidad.stock));
+            return EjectuarNonQuery(insertar);
+        }
+
+        public int Editar(Producto entidad)
+        {
+            parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idproducto", entidad.idProducto));
+            parametros.Add(new SqlParameter("@descripcion", entidad.descripcion));
+            parametros.Add(new SqlParameter("@tipo", entidad.tipo));
+            parametros.Add(new SqlParameter("@precio", entidad.precio));
+            parametros.Add(new SqlParameter("@stock", entidad.stock));
+            return EjectuarNonQuery(actualizar);
+        }
+
+        public int Eliminar(string id)
+        {
+            parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idproducto", id));
+            return EjectuarNonQuery(eliminar);
+        }
+
+        public List<Producto> ObtenerTodo()
+        {
+            var tabla = EjecutarLectura(seleccionarTodo);
+            var listaProducto = new List<Producto>();
+            foreach (DataRow item in tabla.Rows)
+            {
+                listaProducto.Add(new Producto
+                {
+                    idProducto = item[0].ToString(),
+                    descripcion = item[1].ToString(),
+                    tipo = item[2].ToString(),
+                    precio = item[3].ToString(),
+                    stock = item[4].ToString()
+                });
+            }
+            return listaProducto;
+        }
+    }
+}
