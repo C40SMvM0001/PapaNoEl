e20c490a2e47290cca50d6bee9f5731d5a1be68c
VSJose <joseeph159@gmail.com>
2019-12-03 12:01:17 -0400

    agregando clases para cliente

---
+++ PapaNoEl/PapaNoEl/Controlador/ClienteC.cs
@@ -0,0 +1,68 @@
+﻿using PapaNoEl.Modelo.Datos;
+using PapaNoEl.Modelo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapaNoEl.Controlador
+{
+    class ClienteC
+    {
+        ClienteD _db = new ClienteD();
+        public bool GuardarCambios(Cliente nuevoCliente)
+        {
+            try
+            {
+                Cliente tblCliente = new Cliente();
+
+                tblCliente.nombre = nuevoCliente.nombre;
+                tblCliente.apellido = nuevoCliente.apellido;
+                tblCliente.ci= nuevoCliente.ci;
+                tblCliente.tipoempresa = nuevoCliente.tipoempresa;
+                return _db.Adicionar(tblCliente) > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Modificar(Cliente nuevoCliente)
+        {
+            try
+            {
+                Cliente tblCliente = new Cliente();
+
+                tblCliente.nombre = nuevoCliente.nombre;
+                tblCliente.apellido = nuevoCliente.apellido;
+                tblCliente.ci = nuevoCliente.ci;
+                tblCliente.tipoempresa = nuevoCliente.tipoempresa;
+
+                return _db.Editar(tblCliente) > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Eliminar(string id)
+        {
+            try
+            {
+                return _db.Eliminar(id) > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Usuario> MostrarDatos()
+        {
+            return _db.ObtenerTodo().ToList();
+        }
+    }
+}
