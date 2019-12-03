bfff8730cb4b14bce70be4ea6cce357a6c839ee1
C-40S_MvM_001 <b377j9123jaja007@gmail.com>
2019-12-03 04:10:54 +0100

    creando formularios

--- PapaNoEl/PapaNoEl/Vista/frmBase/frmVentana.cs
+++ PapaNoEl/PapaNoEl/Vista/frmBase/frmVentana.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PapaNoEl
+namespace PapaNoEl.Vista.frmBase
 {
-    public partial class Form1 : Form
+    public partial class frmVentana : Form
     {
-        public Form1()
+        public frmVentana()
         {
             InitializeComponent();
         }
